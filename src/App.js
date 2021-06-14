cb7e09f563ed08fe92dfa298995a0e46fee88d95
BraboOliveira <rafaelgsn@live.com>
2021-06-14 00:46:40 -0300

    Novidades

--- src/App.js
+++ src/App.js
@@ -15,9 +15,39 @@ export default function App() {
       <Stack.Navigator>
         {/* <SafeAreaView style={styles.container}> */}
           {/* <Cabecalho/> */}
-          <Stack.Screen name="Home" component={ListaProdutos} />
-          <Stack.Screen name="Sensor Inercial" component={SensorInercial} />
-          <Stack.Screen name="Sensor de Passos" component={SensordePassos} />
+          <Stack.Screen 
+            name="Home" 
+            component={ListaProdutos} 
+            options={{ headerShown: false }} 
+          />
+          <Stack.Screen 
+            name="sensorinercial" 
+            component={SensorInercial} 
+            options={{
+              title: 'Sensor Inercial',
+              headerStyle: {
+                backgroundColor: '#f4511e',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+            />
+          <Stack.Screen 
+            name="sensordepassos"  
+            component={SensordePassos} 
+            options={{
+              title: 'Sensor de Passos',
+              headerStyle: {
+                backgroundColor: '#f4511e',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+            />
         {/* </SafeAreaView> */}
       </Stack.Navigator>
     </NavigationContainer>
--- src/views/components/Cabecalho/data.js
+++ src/views/components/Cabecalho/data.js
@@ -8,7 +8,7 @@ export const DATA = [
       preco: 92.1,
       itemDesc:
         'Contrary to popular belief, Lorem Ipsum is not simply random text',
-      onpress: 'Sensor de Passos',
+      onpress: 'sensordepassos',
     },
     {
       id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
@@ -19,7 +19,7 @@ export const DATA = [
       preco: 123.44,
       itemDesc:
         'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, lorem ipsum',
-      onpress: 'Sensor Inercial',
+      onpress: 'sensorinercial',
     },
     // {
     //   id: '58694a0f-3da1-471f-bd96-145571e29d72',
