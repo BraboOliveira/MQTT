0ffa70468eb7ea5b79dd372f1becdc369ada0f44
BraboOliveira <rafaelgsn@live.com>
2021-06-19 15:07:11 -0300

    a

--- src/views/IMU/sensorinercial.js
+++ src/views/IMU/sensorinercial.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function SensorInercial() {
     return (
         <View>
-            <Text>teste</Text>
+            <Text>Este aplicativo encontra-se em desenvolvimento, entre em contato com o desennvolvedor para mais detalhes!</Text>
         </View>
     )
 }
